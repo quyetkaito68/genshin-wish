470a944bb405f0ad0a6eb0bcd7beec98480c284f
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-04-25 08:01:36 +0700

    add some life func

--- app/src/main/java/com/example/genshinwish/service/Mp3Service.kt
+++ app/src/main/java/com/example/genshinwish/service/Mp3Service.kt
@@ -1,4 +1,23 @@
 package com.example.genshinwish.service
 
-class Mp3Service {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class Mp3Service: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.e("quyetkaito","Service start")
+
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("quyetkaito","Service is destroyed")
+    }
 }
