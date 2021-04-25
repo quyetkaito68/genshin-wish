55b5af6fe1d546208d9b46a1c23952250895b6b4
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-04-25 09:02:35 +0700

    edit application

--- app/src/main/java/com/example/genshinwish/MyApplication.kt
+++ app/src/main/java/com/example/genshinwish/MyApplication.kt
@@ -1,10 +1,26 @@
 package com.example.genshinwish
 
-import androidx.core.app.ActivityCompat
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.content.getSystemService
 
-class MyApplication: ActivityCompat() {
+class MyApplication: Application() {
     companion object{
         const val CHANNEL_ID = "my_channel_id_1"
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(CHANNEL_ID,"Channel Service Example",NotificationManager.IMPORTANCE_DEFAULT)
+            val manager:NotificationManager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(notificationChannel)
+        }
+    }
 }
