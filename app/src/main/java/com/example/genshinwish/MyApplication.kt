0968bf7db5b63eedf594f8f1d9f0416578a2a102
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-04-25 08:55:46 +0700

    add application

---
+++ app/src/main/java/com/example/genshinwish/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.genshinwish
+
+import androidx.core.app.ActivityCompat
+
+class MyApplication: ActivityCompat() {
+    companion object{
+        const val CHANNEL_ID = "my_channel_id_1"
+    }
+
+}
