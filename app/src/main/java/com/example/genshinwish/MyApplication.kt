7f917b7d4a45637495dda46db25f7466fa8b435d
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-04-25 10:23:46 +0700

    add notification test

--- app/src/main/java/com/example/genshinwish/MyApplication.kt
+++ app/src/main/java/com/example/genshinwish/MyApplication.kt
@@ -3,6 +3,7 @@ package com.example.genshinwish
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 import androidx.core.content.getSystemService
 
@@ -19,7 +20,7 @@ class MyApplication: Application() {
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
             val notificationChannel = NotificationChannel(CHANNEL_ID,"Channel Service Example",NotificationManager.IMPORTANCE_DEFAULT)
-            val manager:NotificationManager = getSystemService(NotificationManager::class.java)
+            val manager:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
             manager.createNotificationChannel(notificationChannel)
         }
     }
--- app/src/main/java/com/example/genshinwish/service/Mp3Service.kt
+++ app/src/main/java/com/example/genshinwish/service/Mp3Service.kt
@@ -1,24 +1,50 @@
 package com.example.genshinwish.service
 
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.genshinwish.MyApplication.Companion.CHANNEL_ID
+import com.example.genshinwish.SecondActivity
+import com.example.genshinwish.model.Song
 
 class Mp3Service: Service() {
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return null
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        Log.e("quyetkaito","Service onCreate")
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.e("quyetkaito","Service start")
-
+        val bundle: Bundle? = intent?.getBundleExtra("Bundle")
+        if (bundle!=null){
+            val song = bundle.getSerializable("object_song") as Song
+            Log.e("quyetkaito",song.title)
+            sendNotification(song)
+        }
         return START_NOT_STICKY
+    }
+
+    private fun sendNotification(song: Song) {
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent:PendingIntent = PendingIntent.getActivity(this,0,intent,0)
+        val notify = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentText(song.singer)
+            .setContentTitle(song.title)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1,notify)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("quyetkaito","Service is destroyed")
-        Log.e("quyetkaito","test create branch local, push remote")
+        Log.e("quyetkaito","Service onDestroy")
     }
 }
